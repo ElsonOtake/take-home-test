19180afad0a65e9a97d79547ef384c3b656a1513
Elson Otake <elsonotake@yahoo.com.br>
2022-12-02 19:23:07 -0300

    Add Dafalgan as Pharmacy

--- pharmacy.js
+++ pharmacy.js
@@ -47,6 +47,17 @@ export class Pharmacy {
             }
           }
           break;
+        case "Dafalgan":
+          if (drug.benefit != 0) {
+            drug.benefit -= 2;
+            if (drug.expiresIn < 0) {
+              drug.benefit -= 2;
+            }
+            if (drug.benefit < 0) {
+              drug.benefit = 0;
+            }
+          }
+          break;
         default:
           if (drug.benefit != 0) {
             drug.benefit--;
@@ -59,55 +70,6 @@ export class Pharmacy {
           }
       }
     })
-    // for (var i = 0; i < this.drugs.length; i++) {
-    //   if (
-    //     this.drugs[i].name != "Herbal Tea" &&
-    //     this.drugs[i].name != "Fervex"
-    //   ) {
-    //     if (this.drugs[i].benefit > 0) {
-    //       if (this.drugs[i].name != "Magic Pill") {
-    //         this.drugs[i].benefit = this.drugs[i].benefit - 1;
-    //       }
-    //     }
-    //   } else {
-    //     if (this.drugs[i].benefit < 50) {
-    //       this.drugs[i].benefit = this.drugs[i].benefit + 1;
-    //       if (this.drugs[i].name == "Fervex") {
-    //         if (this.drugs[i].expiresIn < 11) {
-    //           if (this.drugs[i].benefit < 50) {
-    //             this.drugs[i].benefit = this.drugs[i].benefit + 1;
-    //           }
-    //         }
-    //         if (this.drugs[i].expiresIn < 6) {
-    //           if (this.drugs[i].benefit < 50) {
-    //             this.drugs[i].benefit = this.drugs[i].benefit + 1;
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    //   if (this.drugs[i].name != "Magic Pill") {
-    //     this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-    //   }
-    //   if (this.drugs[i].expiresIn < 0) {
-    //     if (this.drugs[i].name != "Herbal Tea") {
-    //       if (this.drugs[i].name != "Fervex") {
-    //         if (this.drugs[i].benefit > 0) {
-    //           if (this.drugs[i].name != "Magic Pill") {
-    //             this.drugs[i].benefit = this.drugs[i].benefit - 1;
-    //           }
-    //         }
-    //       } else {
-    //         this.drugs[i].benefit =
-    //           this.drugs[i].benefit - this.drugs[i].benefit;
-    //       }
-    //     } else {
-    //       if (this.drugs[i].benefit < 50) {
-    //         this.drugs[i].benefit = this.drugs[i].benefit + 1;
-    //       }
-    //     }
-    //   }
-    // }
 
     return this.drugs;
   }
