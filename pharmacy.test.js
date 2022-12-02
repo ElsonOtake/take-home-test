307b3e87cb80b44613b79ab2592059ba4cf264d8
Elson Otake <elsonotake@yahoo.com.br>
2022-12-02 18:25:22 -0300

    Add tests for benefit after expiration date

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -6,4 +6,9 @@ describe("Pharmacy", () => {
       new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()
     ).toEqual([new Drug("test", 1, 2)]);
   });
+  it("should decrease the benefit twice as fast after expiration date", () => {
+    expect(
+      new Pharmacy([new Drug("test", 0, 3)]).updateBenefitValue()
+    ).toEqual([new Drug("test", -1, 1)]);
+  });
 });
